2dde8cbbdc1463df97effae362761df80de2f6b1
Marc Härkönen <harkonen@gatech.edu>
2022-10-02 22:26:22 +0200

    visualize training data

--- train.py
+++ train.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from torchvision.io import read_image
 from torchvision.datasets import ImageFolder
+import torchvision.transforms as T
 
 def show_pic(pic, title):
   plt.title(title)
@@ -14,3 +15,12 @@ pic_name = os.listdir("pics/JP")[0]
 country = pic_name[0:2]
 pic_tensor = read_image(os.path.join("pics", country, pic_name))
 show_pic(pic_tensor, country)
+
+dataset = torchvision.datasets.ImageFolder("pics", T.ToTensor())
+class_idx, class_map = dataset.find_classes(dataset.root)
+
+data_loader = torch.utils.data.DataLoader(dataset,
+                                          batch_size=4,
+                                          shuffle=True)
+inputs, classes = next(iter(data_loader))
+show_pic(torchvision.utils.make_grid(inputs), [class_idx[i] for i in classes])
