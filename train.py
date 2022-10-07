92f988c87001ff97967e27f80451b17f5e7b8652
Marc Härkönen <harkonen@gatech.edu>
2022-10-07 10:37:10 +0200

    random crop 300, batch size 32, 74% on test

--- train.py
+++ train.py
@@ -35,21 +35,26 @@ weights = torchvision.models.ResNet50_Weights.DEFAULT
 
 # Define training dataloader & transforms
 train_dataset = torchvision.datasets.ImageFolder("pics120/train",
-                transform = transforms.Compose([transforms.RandomResizedCrop(256), weights.transforms()]),
+                transform = transforms.Compose([
+                        transforms.RandomCrop(300),
+                        weights.transforms()]),
                 target_transform = transforms.Lambda(lambda c: nn.functional.one_hot(torch.tensor(c), num_classes=len(class_idx))))
 class_idx = train_dataset.classes
 
 train_loader = torch.utils.data.DataLoader(train_dataset,
-                                          batch_size=16,
+                                          batch_size=32,
                                           shuffle=True,
                                           num_workers=4)
 # Visualize
 inputs, classes = next(iter(train_loader))
-show_pic(torchvision.utils.make_grid(un_transform(inputs)), classes)
+show_pic(torchvision.utils.make_grid(un_transform(inputs)), [class_idx[i] for i in classes.argmax(1)])
 
 # Define validation dataloader & transforms
 val_dataset = torchvision.datasets.ImageFolder("pics120/val",
-                transform = weights.transforms(),
+                transform = transforms.Compose([
+                    transforms.FiveCrop(400),
+                    transforms.Lambda(lambda crops: torch.stack([weights.transforms()(crop) for crop in crops])),
+                ]),
                 target_transform = transforms.Lambda(lambda c: nn.functional.one_hot(torch.tensor(c), num_classes=len(class_idx))))
 val_loader = torch.utils.data.DataLoader(val_dataset,
                                           batch_size=10,
@@ -74,7 +79,7 @@ criterion = nn.CrossEntropyLoss()
 
 # Observe that only parameters of final layer are being optimized as
 # opposed to before.
-optimizer_conv = optim.SGD(model_conv.fc.parameters(), lr=0.01, momentum=0.9)
+optimizer_conv = optim.SGD(model_conv.fc.parameters(), lr=0.5, momentum=0.9)
 
 # Decay LR by a factor of 0.1 every 50 epochs
 exp_lr_scheduler = optim.lr_scheduler.StepLR(optimizer_conv, step_size=50, gamma=0.1)
@@ -142,14 +147,16 @@ for epoch in range(num_epochs):
         # forward
         # track history if only in train
         with torch.set_grad_enabled(False):
-            outputs = model(inputs)
-            _, preds = torch.max(outputs, 1)
+            b, cr, co, w, h = inputs.shape
+            outputs = model(inputs.view(-1,co,w,h))
+            outputs = outputs.view(b, cr, -1).mean(1)
             loss = criterion(outputs.softmax(1), labels.to(torch.float32))
 
         # statistics
         running_loss += loss.item() * inputs.size(0)
-        _, label_ind = torch.max(labels, 1)
-        running_corrects += torch.sum(preds == label_ind)
+        preds = outputs.argmax(1)
+        corrects = labels.argmax(1)
+        running_corrects += torch.sum(preds == corrects)
 
     epoch_loss = running_loss / len(val_dataset)
     epoch_acc = running_corrects.double() / len(val_dataset)
@@ -160,22 +167,22 @@ for epoch in range(num_epochs):
 
 # load best model weights
 # model.load_state_dict(best_model_wts)
-torch.save(model.state_dict(), '100_BR_FI_FR_JP_US.pth')
+torch.save(model.state_dict(), '100_BR_FI_FR_JP_US_2.pth')
 
 # Reload model from disk if needed
-model.load_state_dict(torch.load('100_BR_FI_FR_JP_US.pth'))
+model.load_state_dict(torch.load('100_BR_FI_FR_JP_US_2.pth'))
 model.eval()
 
 
 ### Predict new ones?
-test_dataset = torchvision.datasets.ImageFolder("pics120/test",
+test_dataset = torchvision.datasets.ImageFolder("pics120/val",
         transform = transforms.Compose([
                 transforms.FiveCrop(400),
                 transforms.Lambda(lambda crops: torch.stack([weights.transforms()(crop) for crop in crops])),
                 ]),
         target_transform = transforms.Lambda(lambda c: nn.functional.one_hot(torch.tensor(c), num_classes=len(class_idx))))
 test_loader = torch.utils.data.DataLoader(test_dataset,
-                                          batch_size=1,
+                                          batch_size=10,
                                           shuffle=True)
 it = iter(test_loader)
 # Visualize
